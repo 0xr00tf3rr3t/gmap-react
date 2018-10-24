229ad00d9c4b151572b7483af37595d3f8e4ef06
Michael Valentin <Michael.Valentin1136@gmail.com>
2018-10-24 00:41:18 -0400

    feat Add Google Map and read data from Foursquare

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state={
+        venues:[]
+    };
+    componentDidMount() {
+        this.renderMap();
+        this.getVenues();
+    }
+
+    getVenues = () => {
+        const endPoints = "https://api.foursquare.com/v2/venues/explore?";
+        const parameters = {
+            client_id: "543XCRHEDCJN5YQIZONCQ1NR0O3JWIYCFSWS4EKPPZMRPX3U",
+            client_secret: "GUAGXWWAQXHWBJQKLQ3QY5BIWXTF2VWBBHKX10QRLEIOGDBX",
+            query: "food",
+            near: "Sydney",
+            v: "20180323"
+        };
+        axios.get(endPoints + new URLSearchParams(parameters))
+            .then(response => {
+                this.setState(
+                    {
+                       venues: response.data.response.groups[0].items
+                    }
+                );
+                console.log(response)
+            })
+            .catch(error => {
+                    console.log('Error!!' + error);
+                }
+            )
+    };
+    renderMap = () => {
+        loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyA4mGbTg2azsVzhvMhPRY8pTht8HAcD-5g&callback=initMap");
+        window.initMap = this.initMap
+    };
+
+    initMap = () => {
+        let map = new window.google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+    };
+
+    render() {
+        return (
+            <main>
+                <div id="map">
+
+                </div>
+            </main>
+        );
+    }
+}
+
+/*<script ></script> */
+function loadScript(url) {
+    var index = window.document.getElementsByTagName("script")[0];
+    var script = window.document.createElement("script");
+    script.src = url;
+    script.async = true;
+    script.defer = true;
+    index.parentNode.insertBefore(script, index)
+
 }
 
 export default App;
