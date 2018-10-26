e95ca0c7fb0a98bb85074fe318672ddf0198c518
Michael Valentin <Michael.Valentin1136@gmail.com>
2018-10-25 21:54:27 -0400

    feat Markers are working

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 import axios from 'axios'
 
 class App extends Component {
-    state={
-        venues:[]
+    state = {
+        venues: []
     };
     componentDidMount() {
         this.renderMap();
@@ -18,15 +18,15 @@ class App extends Component {
             client_id: "543XCRHEDCJN5YQIZONCQ1NR0O3JWIYCFSWS4EKPPZMRPX3U",
             client_secret: "GUAGXWWAQXHWBJQKLQ3QY5BIWXTF2VWBBHKX10QRLEIOGDBX",
             query: "food",
-            near: "Sydney",
+            near: " Old San Juan",
             v: "20180323"
         };
         axios.get(endPoints + new URLSearchParams(parameters))
             .then(response => {
                 this.setState(
                     {
-                       venues: response.data.response.groups[0].items
-                    }
+                        venues: response.data.response.groups[0].items
+                    }, this.renderMap()
                 );
                 console.log(response)
             })
@@ -35,6 +35,7 @@ class App extends Component {
                 }
             )
     };
+
     renderMap = () => {
         loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyA4mGbTg2azsVzhvMhPRY8pTht8HAcD-5g&callback=initMap");
         window.initMap = this.initMap
@@ -42,23 +43,43 @@ class App extends Component {
 
     initMap = () => {
         let map = new window.google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
+            center: {lat: 18.465518, lng: -66.116316},
+            zoom: 15
         });
+        var infowindow = new window.google.maps.InfoWindow({
+        });
+        this.state.venues.map((myVenue) => {
+            var contentString = `${myVenue.venue.name}`;
+
+                var marker = new window.google.maps.Marker({
+
+                    position: {
+                        lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng
+                    },
+                    map: map,
+                    title: 'San Juan'
+                });
+
+            marker.addListener( 'click', function() {
+                infowindow.setContent(contentString);
+                infowindow.open(map, marker);
+            });
+            }
+        )
     };
+
 
     render() {
         return (
             <main>
                 <div id="map">
-
                 </div>
             </main>
         );
     }
 }
 
-/*<script ></script> */
+
 function loadScript(url) {
     var index = window.document.getElementsByTagName("script")[0];
     var script = window.document.createElement("script");
