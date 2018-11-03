1630de08791476431fa11b6eb360018ede5fa480
Michael Valentin <Michael.Valentin1136@gmail.com>
2018-11-02 23:19:23 -0400

    feat: Can now select proper marker from list

--- src/App.js
+++ src/App.js
@@ -5,15 +5,38 @@ import Map from './Map.js'
 import Menu from './Menu.js'
 
 class App extends Component {
+
     state = {
-        venues: [],
-        map: {},
-        markers: [],
-        infoWindow: {}
+        venues: [],// VEnues to be recieved with API
+        map: {},// Reference to the map
+        markers: [],//List of all markers on map
+        infoWindow: {},//Reference to the infoWindows
+        selectedMarker: null //Reference to the currently selected marker
 
     };
 
-
+    /**
+     * Get's pictures from Flciker
+     */
+    getPictures = () => {
+        const endPoints = "https://api.foursquare.com/v2/venues/?";
+        const parameters = {
+            client_id: "543XCRHEDCJN5YQIZONCQ1NR0O3JWIYCFSWS4EKPPZMRPX3U",
+            client_secret: "GUAGXWWAQXHWBJQKLQ3QY5BIWXTF2VWBBHKX10QRLEIOGDBX",
+            VENUE_ID: "4ca7f3bcf47ea143716f7021"
+        };
+        axios.get(endPoints + new URLSearchParams(parameters)).then(
+            response => {
+                console.log(response);
+            }
+        ).catch(e => {
+            console.log(e)
+        })
+    };
+    /**
+     *  Gets the info from Foursquare
+     * @param query : What's gonna be searched on the server
+     */
     getVenues = (query) => {
         const endPoints = "https://api.foursquare.com/v2/venues/explore?";
         const parameters = {
@@ -25,7 +48,7 @@ class App extends Component {
         };
         axios.get(endPoints + new URLSearchParams(parameters))
             .then(response => {
-                
+
                 this.setState(
                     {
                         venues: response.data.response.groups[0].items
@@ -38,42 +61,60 @@ class App extends Component {
                 }
             )
     };
-
+    /**
+     * Loads the script used to  render the map
+     */
     renderMap = () => {
-
         loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyA4mGbTg2azsVzhvMhPRY8pTht8HAcD-5g&callback=initMap");
         window.initMap = this.initMap;
     };
-
+    /**
+     * Initilize Google Maps elements needed for rendering
+     */
     initMap = () => {
-        let map = new window.google.maps.Map(document.getElementById('map'), {
+        let stateref = this;//Reference to  the window
+        let map = new window.google.maps.Map(document.getElementById('map'), {//Sets the parameters for the map
             center: {lat: 18.465518, lng: -66.116316},
             zoom: 15,
             clickableIcons: false
         });
-        let infowindow = new window.google.maps.InfoWindow({});
-        let newArray = [];
-        this.state.venues.map((myVenue) => {
-                let contentString = `${myVenue.venue.name}`;
+        let infoWindow = new window.google.maps.InfoWindow({});//Initialize the InfoWindows
+        let newArray = [];//holds the new array to be created
+        this.state.venues.map((myVenue) => {  //Gets all venues
 
-                let marker = new window.google.maps.Marker({
+                let contentString = `${myVenue.venue.name}`;//Title on info Windows
+                let marker = new window.google.maps.Marker({//Maps each marker
 
                     position: {
-                        lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng
+                        lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng //Sets the position
                     },
                     map: map,
+                    animation: window.google.maps.Animation.DROP,
                     title: myVenue.venue.name
                 });
                 marker.addListener('click', function () {
-                    console.log(map);
-                    console.log(marker);
-                    infowindow.setContent(contentString);
-                    infowindow.open(map, marker);
+
+                    infoWindow.setContent(contentString);//Change  Name
+                    infoWindow.open(map, marker);// Opens the IW
+                    {
+                        if (stateref.state.selectedMarker !== null && stateref.state.selectedMarker !== marker) {//If the selectedmarker isnt't
+                            stateref.state.selectedMarker.setAnimation(null);         // emptu && is  not already selected
+                        }
+                        if (marker.getAnimation() !== null) {//If animation isn't equal to null
+                            marker.setAnimation(null); // Set null
+                        } else {
+                            marker.setAnimation(window.google.maps.Animation.BOUNCE);//Otherwise make animation bounce
+                        }
+                        stateref.setState({
+                            selectedMarker: marker
+
+                        })
+                    }
                 });
                 newArray.push(marker);
                 this.setState({
                     markers: newArray,
-                    infoWindow: infowindow,
+                    infoWindow: infoWindow,
                     map: map
                 });
 
@@ -82,20 +123,26 @@ class App extends Component {
         );
 
     };
-    setInfoWindow = (marker) => {
-   let mapMarker= this.state.markers.filter(
-       (filter) => {
-       if (filter.title === marker.venue.name) {
-           return filter;
-       }}
-       );
 
+    setInfoWindow = (marker) => {
+
+        let mapMarker = this.state.markers.filter(
+            (filter) => {
+                if (filter.title === marker.venue.name) {
+                    return filter;
+                }
+            }
+        );
 
         this.state.infoWindow.setContent(marker.venue.name);
 
         this.state.infoWindow.open(this.state.map,
             mapMarker[0])
     };
+
+    componentDidMount() {
+        this.getPictures();
+    }
 
     render() {
         return (
@@ -106,7 +153,6 @@ class App extends Component {
             </div>
         );
     }
-
 }
 
 function loadScript(url) {
--- src/Menu.js
+++ src/Menu.js
@@ -12,7 +12,8 @@ class Menu extends Component {
                 {this.props.venues.map((venue)=>
                     {
                         return(
-                            <li onClick= {()=>{this.props.setInfoWindow(venue)}}>
+                            <li onClick= {()=>{
+                                this.props.setInfoWindow(venue)}}>
                                 <p>
                                     {venue.venue.name}
                                 </p>
